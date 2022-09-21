055d5329d5df83a07b19f3d7d6d3ae4a25f80db5
cyxstart <c.163.com>
2022-09-21 09:25:13 +0800

    hot-fix git2

--- src/main/java/com/wish/git.java
+++ src/main/java/com/wish/git.java
@@ -9,5 +9,7 @@ public class git {
         System.out.println("hello,git");
         System.out.println("hello,git");
         System.out.println("hello,hot fix");
+        System.out.println("hello,hot fix1");
+        System.out.println("hello,hot 2");
     }
 }
